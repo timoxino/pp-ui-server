75dba7eda4a8f056ca2580c54e7451e8be95456d
Tsimafei Shchytkavets <Tsimafei_Shchytkavets@epam.com>
2017-05-25 15:29:57 -0400

    use token to talk to resource server

--- src/main/java/com/timoxino/ppoker/service/UserRestService.java
+++ src/main/java/com/timoxino/ppoker/service/UserRestService.java
@@ -3,7 +3,10 @@ package com.timoxino.ppoker.service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpSession;
 import java.security.Principal;
+import java.util.Collections;
+import java.util.Map;
 
 /**
  * Class represents RESTful service to work with {@link java.security.Principal} entity.
@@ -11,10 +14,19 @@ import java.security.Principal;
  * @author timoxino.
  */
 @RestController
-public class UserRestService {
+public class UserRestService
+{
 
     @RequestMapping("/user")
-    public Principal user(Principal user) {
+    public Principal user(Principal user)
+    {
         return user;
     }
+
+    @RequestMapping("/token")
+    public Map<String, String> token(HttpSession httpSession)
+    {
+        return Collections.singletonMap("token", httpSession.getId());
+    }
+
 }
--- src/main/resources/static/js/controller.js
+++ src/main/resources/static/js/controller.js
@@ -47,7 +47,17 @@ app.controller('login', function ($rootScope, $http, $location) {
 
 app.controller('home', function ($http) {
     var self = this;
-    $http.get('http://localhost:9000/').then(function (response) {
-        self.greeting = response.data;
+
+    // replace with an Angular interceptor
+    $http.get('token').then(function (response) {
+        $http({
+            url : 'http://localhost:9000',
+            method : 'GET',
+            headers : {
+                'X-Auth-Token' : response.data.token
+            }
+        }).then(function(response) {
+            self.greeting = response.data;
+        });
     });
 });
